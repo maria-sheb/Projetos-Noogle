0510698a2ec0e75369d9958c5f16f95c191a370a
Maria Sheb <ademir-sabido754@proton.me>
2025-03-25 00:13:17 -0300

    Code Fix Update

--- token.php
+++ token.php
@@ -1,18 +1,1 @@
-<?php
-require 'vendor/autoload.php';
 
-use Firebase\JWT\JWT;
-
-$secretKey = "VERY-ULTRA-BLASTER-MEGA-SUPER-TOKEN!"; 
-
-$payload = [
-    "type" => "visitor",
-    "iat" => time(),
-    "exp" => time() + 36000
-];
-
-$jwt = JWT::encode($payload, $secretKey, 'HS256');
-
-header('Content-Type: application/json');
-echo json_encode(["token" => $jwt]);
-?>
